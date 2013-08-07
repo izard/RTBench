ee655fd943e4bec188272b5e9245392189d05002
Alexander Komarov <izard.ak@gmail.com>
2013-08-07 16:13:28 +0200

    Create isr.c

---
+++ tools/src/disturber/baremetal/smallos/src_c/isr.c
@@ -0,0 +1,86 @@
+/******************************************************************************
+
+  Copyright (c) 2001-2009, Intel Corporation
+  All rights reserved.
+
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions are met:
+
+  1. Redistributions of source code must retain the above copyright notice,
+  this list of conditions and the following disclaimer.
+
+  2. Redistributions in binary form must reproduce the above copyright
+  notice, this list of conditions and the following disclaimer in the
+  documentation and/or other materials provided with the distribution.
+
+  3. Neither the name of the Intel Corporation nor the names of its
+  contributors may be used to endorse or promote products derived from
+  this software without specific prior written permission.
+
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+  POSSIBILITY OF SUCH DAMAGE.
+
+ ******************************************************************************/
+#include <param.h>
+#include <segments.h>
+#include <lwrte_config.h>
+
+isr_t interrupt_handlers[256];
+
+void register_interrupt_handler(u8int n, isr_t handler)
+{
+    interrupt_handlers[n] = handler;
+}
+
+// This gets called from our ASM interrupt handler stub.
+void isr_handler(registers_t regs)
+{
+    if (interrupt_handlers[regs.int_no] != 0)
+    {
+        isr_t handler = interrupt_handlers[regs.int_no];
+        handler(regs);
+    }
+    else
+        kprintf("Recieved unhandled ISR interrupt: %d\n ", regs.int_no);
+}
+
+// This gets called from our ASM interrupt handler stub.
+void irq_handler(registers_t regs)
+{
+    // IRQ7 is a spurious interrupt
+    if (regs.int_no == IRQ7)
+    {
+        //read from In-Service regs
+        outb(0x20, 0x0B); unsigned char irr = inb(0x20);
+        //If bit 7 is not set, return without sending EOI
+        if(!(irr & 0x80)) return;
+    }
+
+    // Send an EOI (end of interrupt) signal to the PICs.
+    // If this interrupt involved the slave.
+    if (regs.int_no >= 40)
+    {
+        // Send reset signal to slave.
+        outb(0xA0, 0x20);
+    }
+    // Send reset signal to master. (As well as slave, if necessary).
+    outb(0x20, 0x20);
+
+    if (interrupt_handlers[regs.int_no] != 0)
+    {
+        isr_t handler = interrupt_handlers[regs.int_no];
+        handler(regs);
+    }
+    else
+        kprintf("Recieved unhandled IRQ interrupt: %d\n ", regs.int_no);
+
+}
