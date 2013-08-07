68806e51706e19946f3c5a09a9f1550277e57cd6
Alexander Komarov <izard.ak@gmail.com>
2013-08-07 15:52:29 +0200

    Create runTest.sh

---
+++ tools/bin/runTest.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# FIXME AK to refactor from write once mode
+if [ $# -ne 3 ]
+then
+    echo "Usage: ./runTest N test disturber"
+    exit -1
+fi
+
+if [ -x $3 ]
+then
+echo $3
+ $3 >/dev/null 2>&1 &
+fi
+
+test -e bin/test.x && NAME=`mktemp` && for i in `seq 1 $1`; do taskset -c 1 bin/test.x >> $NAME ; done && echo -n "Test:" $2 ":" && echo `../../tools/bin/results.sh $NAME` 
+
+killall -9 $3 >/dev/null 2>&1
