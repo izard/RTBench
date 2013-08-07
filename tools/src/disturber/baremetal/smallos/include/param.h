7c94a1c7dcd6a4d19f0381e41d6acceac18b2092
Alexander Komarov <izard.ak@gmail.com>
2013-08-07 16:19:11 +0200

    Create param.h

---
+++ tools/src/disturber/baremetal/smallos/include/param.h
@@ -0,0 +1,62 @@
+/******************************************************************************
+ 
+  Copyright (c) 2001-2009, Intel Corporation
+  All rights reserved.
+ 
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions are met:
+ 
+   1. Redistributions of source code must retain the above copyright notice,
+      this list of conditions and the following disclaimer.
+ 
+   2. Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+ 
+   3. Neither the name of the Intel Corporation nor the names of its
+      contributors may be used to endorse or promote products derived from
+      this software without specific prior written permission.
+ 
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+  POSSIBILITY OF SUCH DAMAGE.
+ 
+******************************************************************************/
+#ifndef __PARAM_H__
+#define __PARAM_H__
+
+typedef unsigned int uint32;
+
+typedef unsigned long long u64;
+typedef unsigned int u32;
+typedef int          int32;
+typedef int          i32;
+typedef int          s32;
+typedef unsigned short uint16;
+typedef unsigned short u16;
+typedef short          i16;
+typedef unsigned char  uint8;
+typedef unsigned char  u8;
+typedef char           s8;
+typedef char           int8;
+
+typedef unsigned int   u32int;
+typedef          int   s32int;
+typedef unsigned short u16int;
+typedef          short s16int;
+typedef unsigned char  u8int;
+typedef          char  s8int;
+#define          NULL  0
+#define          TRUE  1
+#define          FALSE 0
+#define          BASE  0
+
+#endif /* __PARAM_H__ */
